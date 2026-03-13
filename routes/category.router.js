56f3a59e56cbe10c8c20eed46ff44d6de7b5eb59
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-03-13 14:47:52 +0200

    adding validation layer

--- routes/category.router.js
+++ routes/category.router.js
@@ -1,23 +1,29 @@
-const express = require('express');
+const express = require("express");
+
 const {
-    createCategory,
-    getCategories,
-    getCategoryById,
-    updateCategory,
-    deleteCategory
-} = require('../services/category.services');
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+} = require("../services/category.services");
+const {
+  getCategoryValidator,
+  createCategoryValidator,
+  updateCategoryValidator,
+  deleteCategoryValidator,
+} = require("../utils/validators/categoryValidators");
 const router = express.Router();
 
+router
+  .route("/")
+  .get(getCategories)
+  .post(createCategoryValidator, createCategory);
 
-router.route('/')
-    .get(getCategories)
-    .post(createCategory);
-
-
-router.route('/:id')
-    .get(getCategoryById)
-    .patch(updateCategory)
-    .delete(deleteCategory);
-
+router
+  .route("/:id")
+  .get(getCategoryValidator, getCategoryById)
+  .patch(updateCategoryValidator, updateCategory)
+  .delete(deleteCategoryValidator, deleteCategory);
 
 module.exports = router;
---
+++ utils/validators/categoryValidators.js
@@ -0,0 +1,48 @@
+const {check, body} = require('express-validator');
+const validatorMiddleware = require('../../middlewares/validationMiddleware');
+const slugify = require("slugify");
+
+
+const getCategoryValidator = [
+    check('id').isMongoId().withMessage('Invalid category id format'),
+    validatorMiddleware,
+];
+
+
+const createCategoryValidator = [
+    check('name')
+        .notEmpty()
+        .withMessage('Category required')
+        .isLength({min: 3})
+        .withMessage('Too short category name')
+        .isLength({max: 32})
+        .withMessage('Too long category name')
+        .custom((val, {req}) => {
+            req.body.slug = slugify(val);
+            return true;
+        }),
+    validatorMiddleware,
+];
+
+const updateCategoryValidator = [
+    check('id').isMongoId().withMessage('Invalid category id format'),
+    body('name')
+        .optional()
+        .custom((val, {req}) => {
+            req.body.slug = slugify(val);
+            return true;
+        }),
+    validatorMiddleware,
+];
+
+const deleteCategoryValidator = [
+    check('id').isMongoId().withMessage('Invalid category id format'),
+    validatorMiddleware,
+];
+
+module.exports = {
+    getCategoryValidator,
+    deleteCategoryValidator,
+    updateCategoryValidator,
+    createCategoryValidator,
+};
